143ab1802dad3bf5f5defe7beffbe791409f09cf
Ramzan Ali <purvam98@gmail.com>
2018-04-28 02:20:07 -0400

    every saturday

---
+++ README.md
@@ -0,0 +1,20 @@
+# Contructor Hangman Game
+
+This is a terminal line hangman game using Inquirer. Inquirer is a Node npm package that allows you to create terminal line prompts. In this game, the user tries to guess a hidden word and receives feedback from the computer regarding whether or not the guesses are correct. 
+
+This is a game built during the UC Davis Extension coding bootcamp (Trilogy Education Services).
+
+## How to Play
+
+Since this is a terminal line node application, you will need to have the inquirer npm package installed. You can start the game by typing "node index.js" in your terminal. This will prompt you to choose a letter. Continue choosing letters until you either complete the word or run out of guesses. The following is a screenshot of what the game looks like: 
+
+![alt-text](game.png)
+
+## Assignment Challenges
+
+1. The first challenge was having ambiguous instructions that regimented the method we were to build this game. I had to interpret these instructions into practical steps to accomplish the task. Planning and executing your own method appears to be easier than following strict guidelines, given that the guidelines may counter some of your logical processing. 
+2. The second challenge was understanding the modularization at this (small) scale of an application. While it is meant to teach proper file and code structuring, this example was a bit more difficult to grasp because it may not be the most simple or straightforward way of creating this game. 
+
+## Technologies Used
+
+Node.js, npm, JavaScript including object constructors, inquirer npm package
---
+++ index.js
@@ -0,0 +1,67 @@
+var Inquirer = require("inquirer");
+var Word = require("./word");
+var Letter = require("./letter");
+// index.js: The file containing the logic for the course of the game, which depends on Word.js and:
+
+// Randomly selects a word and uses the Word constructor to store it
+// Prompts the user for each guess and keeps track of the user's remaining guesses
+
+var letterGuessedArray = [];
+var previouslyGuessed = [];
+var numberGuesses = 10;
+
+var wordBank = ["mumbai", "chennai", "kolkata", "delhi", "gujarat", "rajasthan", "goa", "assam", "punjab", "banglore"];
+var randomIndex = Math.floor(Math.random() * wordBank.length);
+var randomWord = wordBank[randomIndex];
+// console.log('randomWord');
+// console.log(randomWord);
+
+for(var i = 0; i < randomWord.length; i ++) {
+	letterGuessedArray.push(new Letter(randomWord.charAt(i)));
+}
+
+var currentWord = new Word(letterGuessedArray);
+
+var queryUser = function() {
+	if (numberGuesses > 0) {
+		var wordDisplay = currentWord.show();
+
+		console.log("You have " + numberGuesses + " guesses remaining.");
+		Inquirer.prompt([
+				{
+					name: "letter",
+					message: "Please guess a letter."
+				}
+			]).then(function(userInput) {
+			
+				var guessedLetter = userInput.letter.toLowerCase();
+
+				var alreadyGuessed = false;
+				for (var i = 0; i < previouslyGuessed.length; i++) {
+					if (guessedLetter === previouslyGuessed[i]) {
+						alreadyGuessed = true;
+						break;
+					}
+				}
+
+				if(alreadyGuessed) {
+					console.log("This letter was already guessed.");
+				} else {
+					numberGuesses--;
+					previouslyGuessed.push(guessedLetter);
+					currentWord.trueCharacter(guessedLetter);
+				}
+
+				if(currentWord.finishedWord()) {
+					console.log("Congrats, you've guessed the word correctly!");
+					return;
+				}
+
+				queryUser();
+			});
+	} else {
+		console.log("You've used all of your guesses.");
+	}
+};
+
+queryUser();
